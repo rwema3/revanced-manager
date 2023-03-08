5344530bb8e72b182bcf897f8644ada3b9c90f44
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-03-08 23:35:50 +0200

    Theme builider

--- lib/main.dart
+++ lib/main.dart
@@ -59,6 +59,9 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  
+    return DynamicThemeBuilder(
+      title: 'ReVanced Manager',
       home: const NavigationView(),
       localizationsDelegates: [
         FlutterI18nDelegate(
