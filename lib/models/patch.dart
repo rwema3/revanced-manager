5dc1a68ae553b5f3aff57c743530bd76cd00afdf
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-04-22 23:36:07 +0200

    Json Serializable

--- lib/models/patch.dart
+++ lib/models/patch.dart
@@ -2,3 +2,12 @@ import 'package:json_annotation/json_annotation.dart';
 import 'package:revanced_manager/utils/string.dart';
 
 part 'patch.g.dart';
+
+@JsonSerializable()
+class Patch {
+  final String name;
+  final String description;
+  final String version;
+  final bool excluded;
+  final List<String> dependencies;
+  final List<Package> compatiblePackages;
